5363b45ef7a7d7aea7b2b52e5b5343cc8febb034
santosh2019 <you@example.com>
2018-09-15 04:39:46 +0530

    Frist Commit

---
+++ src/com/scoopen/Customer.java
@@ -0,0 +1,30 @@
+package com.scoopen;
+
+public class Customer {
+	
+	
+	private int custId;
+	private String customerName;
+	private String customerCity;
+	public int getCustId() {
+		return custId;
+	}
+	public void setCustId(int custId) {
+		this.custId = custId;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getCustomerCity() {
+		return customerCity;
+	}
+	public void setCustomerCity(String customerCity) {
+		this.customerCity = customerCity;
+	}
+	
+	
+
+}
